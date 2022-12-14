688f93d51c29bb175b26eed75bc2804c51060edc
Burhan Kuvvetlioglu <bkuvvetlioglu@hotmail.com>
2022-12-14 14:10:56 +0300

    Test1 file modified Test2 file created

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,5 @@
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("This is the first output");
+        System.out.println("This is the first outputt");
     }
 }
